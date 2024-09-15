1b45501040538fe077eb795fdab1cdbabad216d8
NWuSunset <476275@bsd48.org>
2024-09-15 04:47:58 +0100

    Fixed player inputs

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -16,30 +16,48 @@ int main() {
   char board[3][3] ={ {' ',' ',' '},
 		      {' ',' ',' '},
 		      {' ',' ',' '} };
-  int row = 0;
+  int row = -1;
   char rowIn = ' ';
-  int col = 0;
+  int col = -1;
+  char player = 'X'; //X is starting player
 
+  
   makeBoard(board);
-
+  
+  bool correctInput = false;
+  while (correctInput == false) {
   cout << "Enter a row (a-c) and column (1-3)" << endl;
-  //add checking for invalid thigns
-
   cin >> rowIn >> col;
 
   col--; //aline with array starting at 0
+
+  //set the rows to the row numbers in the array
   if (rowIn == 'a') {
     row = 0;
   } else if (rowIn == 'b') {
     row = 1;
   } else if (rowIn == 'c') {
     row = 2;
-  } else {
-    cout << "Invalid row input" << endl;
   }
 
-  cout << row << col;
+  //check for invalid inputs
+  if (row > 2 || row < 0 || col > 2 || col < 0) {
+    cout << "Invalid column. Try again" << endl;
+  } else if (board[row][col] != ' ') {
+    cout << "Invalid move. Try again" << endl;
+  } else {
+    correctInput = true;
+  }
+  
+  }  
+  cout << row << col << endl;
 
+  board[row][col] = player;
+
+  //checkWin; //impliment this later
+
+  //Switch players
+  player = (player == 'X') ? 'O' : 'X'; //if player == X then swtich O if not switch X
   
   return 0;
 }
